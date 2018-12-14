4026d235daedfba820064a263b44768eeadb75d9
Emilijus <slibinas2012@gmail.com>
2018-12-14 10:01:57 +0200

    Added tank movement component

---
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankMovementComponent.h"
+
+void UTankMovementComponent::IntendMoveFoward(float Throw)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Intend move foward throw:  %f"), Throw);
+}
